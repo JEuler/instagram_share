bbbf82eab1f35c0bd257dad564bbbcf8eec8322f
Ivan Terekhin <i.terhin@gmail.com>
2020-04-20 19:02:38 +0300

    iOS hare (WIP)

--- lib/instagram_share.dart
+++ lib/instagram_share.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:image_downloader/image_downloader.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InstagramShare {
   static const MethodChannel _channel = const MethodChannel('instagramshare');
@@ -11,10 +15,29 @@ class InstagramShare {
   /// [type]  "image" ,"video"
   static Future<void> share(String path, String type) {
     assert(path != null && path.isNotEmpty);
-    final Map<String, dynamic> params = <String, dynamic>{
-      'path': path,
-      'type': type,
-    };
-    return _channel.invokeMethod('share', params);
+    if (Platform.isAndroid) {
+      final Map<String, dynamic> params = <String, dynamic>{
+        'path': path,
+        'type': type,
+      };
+      return _channel.invokeMethod('share', params);
+    } else {}
+  }
+
+  Future<void> _shareMediaIOS(String filePath) async {
+    if (await Permission.photos.request().isGranted) {
+      final mediaId = await ImageDownloader.downloadImage(filePath);
+      if (mediaId == null) {}
+      final instagramUrl = 'instagram://library?LocalIdentifier=$mediaId';
+      if (await canLaunch(instagramUrl)) {
+        final instagramUrl = 'instagram://library?LocalIdentifier=$mediaId';
+        await launch(instagramUrl);
+      } else {}
+    } else {
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        /// Let's open app settings on
+        openAppSettings();
+      });
+    }
   }
 }
